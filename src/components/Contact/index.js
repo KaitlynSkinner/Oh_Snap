a79bfc49593cfc6ea2349dba8037e2d42e56fab8
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-05-20 14:25:56 -0400

    added change events and form submission

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const { name, email, message } = formState;
+
+    // handlechange events for input and textarea
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+
+    // handlesubmit for the form submission
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+    
+    // console.log(formState);
 
     // JSX
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
-                // name input
+            <form id="contact-form" onSubmit={handleSubmit}>
+                {/* name input */}
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
-                // email input
+                {/* email input */}
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
                 </div>
-                // message text area
+                {/* message text area */}
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5"  />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
                 </div>
                 <button type="submit">Submit</button>
             </form>
