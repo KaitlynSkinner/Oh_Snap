dcb6d86b03a14a0400162983424d316aa561c7e3
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-05-20 16:19:04 -0400

    tests added for contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -42,7 +42,7 @@ function ContactForm() {
     // JSX
     return (
         <section>
-            <h1>Contact me</h1>
+            <h1 data-testId="h1tag">Contact me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
                 {/* name input */}
                 <div>
@@ -66,7 +66,7 @@ function ContactForm() {
                         <p className="error-text">{errorMessage}</p>
                     </div>
                 )}
-                <button type="submit">Submit</button>
+                <button data-testId="button" type="submit">Submit</button>
             </form>
         </section>
     )
