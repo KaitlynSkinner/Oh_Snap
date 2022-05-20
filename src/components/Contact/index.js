9463d53fbc99ba8d8de42cca51b0d9db52b589a8
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-05-20 15:25:43 -0400

    contact form complete

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
     const { name, email, message } = formState;
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     // handlechange events for input and textarea
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value})
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+              } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                }
+              }
+            //   console.log('errorMessage', errorMessage);
+          }  
+          
+          if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value});
+          }
     }
 
     // handlesubmit for the form submission
@@ -15,7 +36,7 @@ function ContactForm() {
         e.preventDefault();
         console.log(formState);
     }
-    
+
     // console.log(formState);
 
     // JSX
@@ -26,18 +47,25 @@ function ContactForm() {
                 {/* name input */}
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                    {/* onBlur attribute will fire the event once the user has changed focus from the input field, thus allowing the user to finish their entry before validating their input. */}
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
                 </div>
                 {/* email input */}
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+                    <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
                 </div>
                 {/* message text area */}
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
+                    <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5"  />
                 </div>
+                {/* f the first value resolves to true, the second expression is evaluated. */}
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
